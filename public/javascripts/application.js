18f4d5cd55597ef5cf16f205c0baf88d948f7e0d
Balint Erdi <balint.erdi@gmail.com>
2008-12-07 17:23:47 +0100

    using jquery autocomplete plugin for autocompletion of recommendee name
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,8 @@
 // Common JavaScript code across your application goes here.
-function addAjaxUpdater() {
-	$("#recommendation_recommendee_name").keyup(function() {
-		$.get("/users/user_names", { 'name' : this.value }, function(data) {
-			if (data) $("ul#recommendee_names").html("<li>"+data+"</li>");	
-		});
-	});
+function addAjaxAutoCompleter() {
+	$("#recommendation_recommendee_name").autocomplete("/users/user_names");
 }
-
-function addSimpleUpdater() {
-	$("#recommendation_recommendee_name").keyup(function() {
-		$("ul#recommendee_names").append("<li>"+this.value+"</li>");
-	});
-}
-
 
 $(document).ready(function(){
-	addAjaxUpdater();
-	// addSimpleUpdater();
+	addAjaxAutoCompleter();
 })
