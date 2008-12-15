19a791a224b9811429f566a2431dc7d7fb750f36
Balint Erdi <balint.erdi@gmail.com>
2008-12-15 14:44:14 +0100

    creating sidebar links dynamically
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,18 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.  
+    # helpers defined here available to all views.
+    def left_sidebar_links
+      links = []
+      if session.authenticated?
+        user = session.user
+        links << { :url => resource(user), :text => "View my profile" }
+        links << { :url => resource(user, :edit), :text => "Edit my profile" }
+        links << { :url => url(:user_search), :text => "Search users" }
+        links << { :url => url(:logout), :text => "Logout" }
+      else
+        links << { :url => url(:new_user), :text => "Register"}
+      end
+      links        
+    end
   end
 end
